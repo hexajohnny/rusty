feb0fa48bb4df6b78860f2a7e31501742f077d78
virtualmassacre <whereisjohnwhenineedhim@gmail.com>
2026-02-10 11:49:35 -0500

    Set author/company metadata

--- build.rs
+++ build.rs
@@ -3,6 +3,11 @@ fn main() {
     {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/icon.ico");
+        // Populate Windows version info so the binary has sensible metadata in Explorer.
+        res.set("CompanyName", "hexajohnny");
+        res.set("LegalCopyright", "Copyright (c) hexajohnny");
+        res.set("FileDescription", "Rusty SSH");
+        res.set("ProductName", "Rusty SSH");
         res.compile().unwrap();
     }
 }
